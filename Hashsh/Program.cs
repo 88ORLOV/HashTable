03a8294c3ddb801c9e35e5ccb5e7964e1f3787a3
88ORLOV <44868289+88ORLOV@users.noreply.github.com>
2019-03-28 21:00:48 +0500

    Add files via upload

---
+++ Hashsh/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Hash
+{	
+    public class Program
+    {
+        class DataHash
+        {
+            public readonly int Key;
+            public object Value;
+            public DataHash(int key, object value)
+            {
+                Key = key;
+                Value = value;
+            }
+        }
+        public class HashTable
+        {
+            private List<DataHash>[] list;
+
+            public HashTable(int size)
+            {
+                list = new List<DataHash>[size];
+            }
+
+            public void PutPair(object key, object value)
+            {
+                var keyHashCode = key.GetHashCode();
+                var id = Math.Abs(keyHashCode) % list.Length;
+
+                if (list[id] == null)
+                {
+                    list[id] = new List<DataHash> { new DataHash(keyHashCode, value) };
+                }
+                else
+                {
+                    var val = list[id].FirstOrDefault(x => x.Key == keyHashCode);
+
+                    if (val != null)
+                        val.Value = value;
+                    else
+                        list[id].Add(new DataHash(keyHashCode, value));
+                }
+            }
+
+
+            public object GetValueByKey(object key)
+            {
+                try
+                {
+                    var keyHashCode = key.GetHashCode();
+                    var value = list[Math.Abs(keyHashCode) % list.Length];
+                    return value.Find(x => x.Key == keyHashCode).Value;
+                }
+                catch
+                {
+                    return null;
+                }
+            }      
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }  
+}
+
+
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Hash.Program;
+
+namespace Hash.Tests
+{
+    [TestClass]
+    public class HashTests
+    {
+        [TestMethod]
+        public void HashTest3()
+        {
+            HashTable hash = new HashTable(3);
+
+            var element1 = 8;
+            var element2 = new[] { 1, 2, 3, 4 };
+            var element3 = "8";
+
+            hash.PutPair(0, element1);
+            hash.PutPair(30, element2);
+            hash.PutPair("arr", element3);
+           
+
+            Assert.AreEqual(element1, hash.GetValueByKey(0));
+            Assert.AreEqual(element2, hash.GetValueByKey(30));
+            Assert.AreEqual(element3, hash.GetValueByKey("arr"));
+        }
+
+        [TestMethod]
+        public void HashTest2()
+        {
+            HashTable hash = new HashTable(2);
+
+            var key = 15;
+            var element1 = 3;
+            var element2 = 9;
+
+            hash.PutPair(key, element1);
+            hash.PutPair(key, element2);
+
+            Assert.AreEqual(element2, hash.GetValueByKey(key));
+        }
+
+        [TestMethod]
+        public void HashTest10000n1()
+        {
+            int findKey = 0, flag, findElement = 0;
+            var size = 10000;
+            var rnd = new Random();
+            flag = rnd.Next(size);
+            var hash = new HashTable(size);
+            for (var i = 0; i < size; i++)
+            {
+                var element = rnd.Next();
+                var key = rnd.Next();
+                hash.PutPair(key, element);
+                if (i == flag)
+                {
+                    findElement = element;
+                    findKey = key;
+                }
+            }
+            Assert.AreEqual(findElement, hash.GetValueByKey(findKey));
+
+        }
+
+        [TestMethod]
+        public void HashTest10000n1000()
+        {
+            int flag;
+            var size = 10000;
+            var rnd = new Random();
+            flag = rnd.Next(size);
+            var hash = new HashTable(size);
+            for (var i = 0; i < size; i++)
+            {
+                var element = rnd.Next();
+                hash.PutPair(i, element);
+            }
+            for (var i = 0; i < 1000; i++)
+                Assert.AreEqual(null, hash.GetValueByKey(rnd.Next(1000) + 10000));
+
+        }
+    }
+}
+
